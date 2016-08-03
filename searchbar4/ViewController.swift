e782f657e90eb6bc1b47194c1af5e2cf2e4b267c
GEGWC8 <GEGWC8@administrators-macbook-air-3.local>
2016-08-03 11:17:28 -0700

    Initial Commit

---
+++ searchbar4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  searchbar4
+//
+//  Created by GEGWC8 on 8/3/16.
+//  Copyright © 2016 GEGWC8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
